4e4eb8e3ba446841620f820b310ee859f3914fe5
Crowdbotics <team@crowdbotics.com>
2021-07-25 05:40:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/arvitech_29145/MainActivity.java
@@ -0,0 +1,15 @@
+package com.arvitech_29145;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "arvitech_29145";
+  }
+}
